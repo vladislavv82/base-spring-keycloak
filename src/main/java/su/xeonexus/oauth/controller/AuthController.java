4fd352d6e173fa12267ea8fd914af981d1ca743e
vladislavv82 <vladislav.akopian82@gmail.com>
2024-11-07 19:16:34 +0300

    auth controller

---
+++ src/main/java/su/xeonexus/oauth/controller/AuthController.java
@@ -0,0 +1,50 @@
+package su.xeonexus.oauth.controller;
+
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import su.xeonexus.oauth.dto.AuthDTO;
+
+@Setter
+@RestController
+public class AuthController {
+
+    @Value("${client-id}")
+    private String clientId;
+
+    @Value("${resource-url}")
+    private String resourceServerUrl;
+
+    @Value("${grant-type}")
+    private String grantType;
+
+    @GetMapping("/auth")
+    public String auth(@RequestBody AuthDTO authDTO) {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        var body = "client_id=" + clientId +
+                "&username=" + authDTO.login() +
+                "&password=" + authDTO.password() +
+                "&grant_type=" + grantType;
+
+        var requestEntity = new HttpEntity<>(body, headers);
+        var restTemplate = new RestTemplate();
+
+        var responce = restTemplate.exchange(
+                resourceServerUrl,
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+        if (responce.getStatusCode().value() == 200) {
+            return responce.getBody();
+        }
+        return null;
+    }
+
+}
